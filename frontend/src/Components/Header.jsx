43eac985422757f38e453351cd0c80085482fc10
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-04-02 08:44:16 +0530

    Header:Updated

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <h1
+      style={{
+        justifyContent: "center",
+        display: "flex",
+        background: "black",
+        color: "white",
+        paddingTop: 20,
+        margin: 0,
+        height: 50,
+        fontFamily: "monospace",
+      }}
+    >
+      Decentralized Collaboration Platform
+    </h1>
+  );
+};
+
+export default Header;
